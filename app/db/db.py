39bcda22c33090a90f922f778ee553bbaa9aff51
mrkorf <sashatimchuk2000@gmail.com>
2022-10-19 11:29:43 +0300

    create user_table with alembic

--- app/db/db.py
+++ app/db/db.py
@@ -1,10 +1,11 @@
+import os
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from config import settings
 
-DATABASE_URL = "postgresql://postgres:postgres@172.17.0.1:5432/postgres"
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
