18f9ad1f9062673bb63847ab47801729ba251728
mrkorf <sashatimchuk2000@gmail.com>
2022-10-19 11:34:07 +0300

    small changes in alembic versions

--- migration/versions/001_create_table_user.py
+++ migration/versions/001_create_table_user.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'create_table_user'
+revision = '001_create_table_user'
 down_revision = None
 branch_labels = None
 depends_on = None
